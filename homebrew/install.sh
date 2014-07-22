37e89dc98eb078f71d2a724f3f680a3e0eb75f66
d <dunc@d.lan>
2014-07-22 20:27:09 +0100

    add home files, install script to setup

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -3,6 +3,7 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 install coreutils
+install tree
 install git
 install go
 
---
+++ prezto/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+zsh
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+chsh -s /bin/zsh
+
