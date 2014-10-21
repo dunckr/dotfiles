ef6bcd4461f7a659799ef3b8dc6be621505c0b66
Duncan Beaton <dunckr@gmail.com>
2014-10-21 22:58:05 +0100

    add custom prompt git clone

--- prezto/install.sh
+++ prezto/install.sh
@@ -4,3 +4,5 @@ zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 chsh -s /bin/zsh
 
+git clone https://github.com/dunckr/swash-prompt "${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions"
+
