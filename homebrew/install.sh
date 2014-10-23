8c1381f6786de83247dfa023ba79b1d7b0b6f9e1
Duncan Beaton <dunckr@gmail.com>
2014-10-23 18:17:47 +0100

    add homebrew casks

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,8 +2,42 @@
 
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
-install coreutils
-install tree
-install git
-install go
+TAPS=(
+    caskroom/cask
+)
+FORMULAS=(
+    caskroom/cask/brew-cask
+    coreutils
+    docker
+    go
+    git
+    python
+    python3
+    tree
+)
+CASKS=(
+    alfred
+    dash
+    dropbox
+    firefox
+    google-chrome
+    iterm2
+    node
+    onepassword
+    sketch
+    skype
+    spotify
+    sublime-text3
+    vlc
+)
 
+for tap in ${TAPS[@]}
+do
+    brew tap $tap
+done
+
+brew install ${FORMULAS[@]}
+brew update
+brew cask install ${CASKS[@]}
+brew cask alfred link
+brew cleanup
