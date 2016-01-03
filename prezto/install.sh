892d8b544ea60cd993a0875905bd2367f7ad98c9
Duncan Beaton <dunckr@gmail.com>
2016-01-03 12:19:37 +0000

    changes to jshint

--- prezto/install.sh
+++ prezto/install.sh
@@ -3,6 +3,3 @@
 zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 chsh -s /bin/zsh
-
-git clone https://github.com/dunckr/swash-prompt "${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions"
-
