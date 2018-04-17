7f07af6e80b0c8b93d229c4b1020f65070badaac
Duncan Beaton <dunckr@gmail.com>
2018-04-17 21:12:16 +0100

    rm slate; update eslint defaults

--- home/.eslintrc.js
+++ home/.eslintrc.js
@@ -2,15 +2,18 @@ module.exports = {
 	extends: 'airbnb',
 	env: {
 		browser: true,
-		mocha: true,
+		jest: true,
 		node: true,
 	},
 	rules: {
+		'arrow-parens': [2, 'as-needed'],
 		'comma-dangle': 0,
 		'no-tabs': 0,
+		'object-curly-newline': 0,
 		'react/jsx-filename-extension': 0,
 		'react/jsx-indent': 0,
 		'react/jsx-indent-props': 0,
+		'space-before-function-paren': 0,
 		indent: [2, 'tab'],
 	},
 };
