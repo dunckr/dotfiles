7e29122f045d68716d85c10c87d0343077219f72
Duncan Beaton <dunckr@gmail.com>
2025-08-13 14:43:48 +0100

    switch to firefox

--- home/.phoenix.js
+++ home/.phoenix.js
@@ -9,8 +9,8 @@ const bindLaunch = (appName, key, modifiers) =>
     app.isActive() ? app.hide() : app.focus();
   });
 
-bindLaunch('Google Chrome', 'c', ['cmd', 'shift']);
-// bindLaunch('Firefox', 'c', ['alt', 'shift']);
+bindLaunch('Firefox', 'c', ['cmd', 'shift']);
+bindLaunch('Chrome', 'c', ['alt', 'shift']);
 bindLaunch('Safari', 'c', ['cmd', 'ctrl']);
 bindLaunch('Notes', 'n', ['alt', 'shift']);
 bindLaunch('Slack', 'm', ['alt', 'shift']);
